5e30d1f4d1ef7ccbe3ed83c124d5a3c723e6e334
rajpatelbot <rajpatel158g@gmail.com>
2023-07-21 22:32:31 +0530

    feat: Added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import HomePage from "./pages/Home.page";
+
 const App = () => {
-  return <>App</>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/language-choice" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
