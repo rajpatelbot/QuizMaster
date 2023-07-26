1b95b0cea9aae6b9561102d8eb39bdaf7b5eae40
rajpatelbot <rajpatel158g@gmail.com>
2023-07-27 00:42:21 +0530

    fix: Solved connection timeoup problem

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,27 +1,25 @@
-import { MongoClient, MongoClientOptions } from "mongodb";
+import mongoose from "mongoose";
+import { MongoClientOptions } from "mongodb";
 import { DB_USERNAME, DB_PASSWORD, CLUSTER_URL, DB_NAME } from "./env";
 
-if (!DB_USERNAME || !DB_PASSWORD || !CLUSTER_URL) {
-  throw new Error("DB_USERNAME or DB_PASSWORD or CLUSTER_URL is not defined");
+if (!DB_USERNAME || !DB_PASSWORD || !CLUSTER_URL || !DB_NAME) {
+  throw new Error(`Missing environment variables for MongoDB connection`);
 }
 
-const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}/${DB_NAME}?retryWrites=true&w=majority`;
 
 async function connectToDB() {
-  const options: MongoClientOptions = {
+  const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   } as MongoClientOptions;
 
-  const client = new MongoClient(MONGODB_URI, options);
-
   try {
-    await client.connect();
+    await mongoose.connect(MONGODB_URI, options);
     console.log("⚡️[DB]: Connected successfully!");
-    return client.db(DB_NAME);
   } catch (error) {
     console.log(`❌[DB]: Could not connect. Here is the error: ${error as string}`);
-    process.exit(1);
+    process.exit();
   }
 }
 
